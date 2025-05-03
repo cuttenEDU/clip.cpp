caff314abdb2b23b1867967ab635dbdb01067eec
RealMetamorph <andr.timchuk@yandex.ru>
2025-05-03 17:25:21 +0300

    fix exports

--- clip.h
+++ clip.h
@@ -4,6 +4,15 @@
 #include <cstdint>
 
 struct clip_ctx;
+#if defined(CLIP_SHARED)
+#if defined(CLIP_BUILD)
+#define CLIP_DLLPORT __declspec(dllexport)
+#else
+#define CLIP_DLLPORT __declspec(dllimport)
+#endif
+#else
+#define CLIP_DLLPORT
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -38,13 +47,13 @@ extern "C" {
         size_t size;
     };
 
-    clip_ctx *clip_model_load(const char *fname, int verbosity);
+    CLIP_DLLPORT clip_ctx *clip_model_load(const char *fname, int verbosity);
 
-    void clip_free(const clip_ctx *ctx);
+    CLIP_DLLPORT void clip_free(const clip_ctx *ctx);
 
-    clip_text_hparams *clip_get_text_hparams(clip_ctx *ctx);
+    CLIP_DLLPORT clip_text_hparams *clip_get_text_hparams(clip_ctx *ctx);
 
-    clip_vision_hparams *clip_get_vision_hparams(clip_ctx *ctx);
+    CLIP_DLLPORT clip_vision_hparams *clip_get_vision_hparams(clip_ctx *ctx);
 
     // RGB uint8 image
     struct clip_image_u8 {
@@ -73,52 +82,56 @@ extern "C" {
         size_t size;
     };
 
-    bool clip_tokenize(const clip_ctx *ctx, const char *text, clip_tokens *tokens);
+    CLIP_DLLPORT bool clip_tokenize(const clip_ctx *ctx, const char *text, clip_tokens *tokens);
 
-    clip_image_u8 *clip_image_u8_make();
+    CLIP_DLLPORT clip_image_u8 *clip_image_u8_make();
 
-    clip_image_f32 *clip_image_f32_make();
+    CLIP_DLLPORT clip_image_f32 *clip_image_f32_make();
 
-    void clip_image_u8_clean(clip_image_u8 *img);
+    CLIP_DLLPORT void clip_image_u8_clean(clip_image_u8 *img);
 
-    void clip_image_f32_clean(clip_image_f32 *res);
+    CLIP_DLLPORT void clip_image_f32_clean(clip_image_f32 *res);
 
-    void clip_image_u8_free(clip_image_u8 *img);
+    CLIP_DLLPORT void clip_image_u8_free(clip_image_u8 *img);
 
-    void clip_image_f32_free(clip_image_f32 *res);
+    CLIP_DLLPORT void clip_image_f32_free(clip_image_f32 *res);
 
-    bool clip_image_load_from_file(const char *fname, clip_image_u8 *img);
+    CLIP_DLLPORT bool clip_image_load_from_file(const char *fname, clip_image_u8 *img);
 
-    bool clip_image_preprocess(const clip_ctx *ctx, const clip_image_u8 *img, clip_image_f32 *res);
+    CLIP_DLLPORT bool clip_image_preprocess(const clip_ctx *ctx, const clip_image_u8 *img, clip_image_f32 *res);
 
-    bool clip_text_encode(const clip_ctx *ctx, int n_threads, const clip_tokens *tokens, float *vec, bool normalize);
+    CLIP_DLLPORT bool clip_text_encode(
+        const clip_ctx *ctx, int n_threads, const clip_tokens *tokens, float *vec, bool normalize
+    );
 
-    bool clip_image_encode(const clip_ctx *ctx, int n_threads, clip_image_f32 *img, float *vec, bool normalize);
+    CLIP_DLLPORT bool clip_image_encode(
+        const clip_ctx *ctx, int n_threads, clip_image_f32 *img, float *vec, bool normalize
+    );
 
-    void clip_image_batch_preprocess(
+    CLIP_DLLPORT void clip_image_batch_preprocess(
         const clip_ctx *ctx, int n_threads, const clip_image_u8_batch *img_inputs, clip_image_f32_batch *imgs_resized
     );
 
-    bool clip_image_batch_encode(
+    CLIP_DLLPORT bool clip_image_batch_encode(
         const clip_ctx *ctx, int n_threads, const clip_image_f32_batch *imgs, float *vec, bool normalize
     );
 
     // bool image_normalize(const clip_image_u8 *img, clip_image_f32 *res);
 
-    bool clip_compare_text_and_image(
+    CLIP_DLLPORT bool clip_compare_text_and_image(
         const clip_ctx *ctx, int n_threads, const char *text, const clip_image_u8 *image, float *score
     );
 
-    float clip_similarity_score(const float *vec1, const float *vec2, int vec_dim);
+    CLIP_DLLPORT float clip_similarity_score(const float *vec1, const float *vec2, int vec_dim);
 
-    bool softmax_with_sorting(float *arr, int length, float *sorted_scores, int *indices);
+    CLIP_DLLPORT bool softmax_with_sorting(float *arr, int length, float *sorted_scores, int *indices);
 
-    bool clip_zero_shot_label_image(
+    CLIP_DLLPORT bool clip_zero_shot_label_image(
         clip_ctx *ctx, int n_threads, const clip_image_u8 *input_img,
         const char **labels, size_t n_labels, float *scores, int *indices
     );
 
-    bool clip_model_quantize(const char *fname_inp, const char *fname_out, int itype);
+    CLIP_DLLPORT bool clip_model_quantize(const char *fname_inp, const char *fname_out, int itype);
 
     #ifdef __cplusplus
 }
